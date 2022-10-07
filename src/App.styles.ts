6785a61021413aa16f4b45c8a02aae7e87361c06
Tiago Moita <t.moita@sportscorner.com.au>
2022-10-07 15:31:08 +0100

    Added fonts

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -1,10 +1,23 @@
 import styled from "styled-components";
+import UrbanistRegular from './assets/fonts/Urbanist-Regular.ttf'
 
 const styles = styled.main`
   height: 100vh;
   display: flex;
   flex-direction: column;
   
+  @font-face {
+    font-family: 'Urbanist Regular';
+    src: local('Urbanist Regular'), local('Urbanist Regular'), url(${UrbanistRegular}) format('truetype');
+  }
+
+  body, p, a {
+    font-family: 'Urbanist Regular';
+  }
+
+  a {
+    font-family: 'Urbanist Regular';
+  }
  
 `;
 
